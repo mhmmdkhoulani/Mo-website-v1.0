5fa7f6744826367d7a21849d028e7195c42f0142
mhmmdkhoulani <mhmmdkholani@gmail.com>
2020-08-18 17:51:06 +0300

    Updates

---
+++ precache-manifest.6fc260eda594c784b6dbfc862e33ead6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb679cb62bf3b0d2460942fc5e7f0d81",
+    "url": "/Mo-website-v1.0/index.html"
+  },
+  {
+    "revision": "08a6f0a378f6bf4b8716",
+    "url": "/Mo-website-v1.0/static/css/2.64930493.chunk.css"
+  },
+  {
+    "revision": "73f0c085a4bf3f809d67",
+    "url": "/Mo-website-v1.0/static/css/main.3a8332d6.chunk.css"
+  },
+  {
+    "revision": "08a6f0a378f6bf4b8716",
+    "url": "/Mo-website-v1.0/static/js/2.051ec48f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Mo-website-v1.0/static/js/2.051ec48f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73f0c085a4bf3f809d67",
+    "url": "/Mo-website-v1.0/static/js/main.cb50dd77.chunk.js"
+  },
+  {
+    "revision": "b93358c9173138302798",
+    "url": "/Mo-website-v1.0/static/js/runtime-main.94040b3f.js"
+  },
+  {
+    "revision": "885bfb41a8fe2f8a02bcb901e6de3967",
+    "url": "/Mo-website-v1.0/static/media/job-1.885bfb41.png"
+  },
+  {
+    "revision": "af40a36ad52310c6631bb624cfb4e4e2",
+    "url": "/Mo-website-v1.0/static/media/logo.af40a36a.svg"
+  },
+  {
+    "revision": "510b649a8826cb478d6af94d83ff3400",
+    "url": "/Mo-website-v1.0/static/media/profile.510b649a.jpg"
+  }
+]);
